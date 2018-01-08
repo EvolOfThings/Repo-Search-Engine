608db082d6f98184f19bed72435e4ceae890c491
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-01-08 22:06:59 +0530

    first component

--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
+
+class RepoSearchBar extends React.Component {
+  render() {
+    const repoName = this.props.repoName;
+    return (
+      <form>
+        <input type="text" placeholder="Repository name"/>
+      </form>
+      );
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
