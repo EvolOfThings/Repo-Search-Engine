557275be23314696cc846fb0430a3658a909766d
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-01-20 20:05:23 +0530

    component for repo results UI

---
+++ src/RepoResults.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class RepoResult extends Component {
+    mount(){
+        ReactDOM.render(<App />, document.getElementById('results'))
+    }
+    render(){
+        return (
+            <div>
+            <label> Name of a repository:
+            <input type="text" onChange={this.mount.bind(this)}/>
+            </label>
+                <div id="results"></div>
+            </div>
+        )
+    }
+}
+
+
+
+export default RepoResult;
