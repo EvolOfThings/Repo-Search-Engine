35cedf135baae48b9b60be5c374f56fd11b0b034
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-01-30 18:28:36 +0530

    component for usersearch

---
+++ src/RepoSearch.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+
+let Profile = (
+    <div id="RepoResult">
+        <div className='profilePic'>
+            <img src='#' />
+        </div>
+        <div className="profileInfo">
+            <label> username:
+            <p> username is fetched </p>
+            </label>
+            <label> repository:
+            <p> repository link </p>
+            </label>
+        </div>
+    </div>
+    );
+
+class RepoSearch extends Component {
+    mount(){
+        ReactDOM.render(<App />, document.getElementById('results'))
+    }
+    render(){
+        return (
+            <div>
+            <label> Name of a repository:
+            <input type="text" onChange={this.mount.bind(this)}/>
+            </label>
+            </div>
+        )
+    }
+}
+
+
+
+
+export default RepoSearch;
---
+++ src/UserSearch.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+
+class UserSearch extends Component {
+    render() {
+        return (
+            <div>
+                <div className='profilePic'>
+                    <img src='#' />
+                </div>
+                <div className='repos'>
+                    <ul>
+                    <li>repo A </li>
+                    <li>repo B </li>
+                    <li>repo C </li>
+                    </ul>
+                </div>
+            </div>
+            );
+    }
+}
+
+
+export default UserSearch;
