09e96868f0f448bab240c943556079349890fb90
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-02-14 18:23:48 +0530

    grid system for the image and repos

--- src/UserSearch.js
+++ src/UserSearch.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-
+import {Grid, Row, Col, Image} from 'react-bootstrap';
 
 let Repository = () => {
     return (
-    <div className='repos'>
+    <div className='Repos'>
         <ul>
         <li className='repoItem'>repo A </li>
         <li className='repoItem'>repo B </li>
@@ -15,13 +15,17 @@ let Repository = () => {
 class UserSearch extends Component {
     render() {
         return (
-            <div className='profile'>
-                <div className='profileImage'>
-                    <img className='profilePic' src='#' />
-                </div>
-                <div>
-                    <Repository />
-                </div>
+            <div className='Profile'>
+                <Grid>
+                    <Row>
+                        <Col sm={4} className='profileImage'>
+                            <Image className='profilePic' src='#' rounded />
+                        </Col>
+                        <Col sm={8} >
+                            <Repository />
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
             );
     }
