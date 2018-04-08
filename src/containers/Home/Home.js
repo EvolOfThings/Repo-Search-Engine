3f1c7ae0eb944ff0f9aad3438846cb8d86d52fd7
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-04-08 08:08:35 +0530

    Home page

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Git Repo Search</h2>    
+             </div>
+        );
+    }
+}
+
+export default Home;
