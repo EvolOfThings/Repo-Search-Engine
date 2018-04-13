60d14d1dd8a4891ca531ee38f533ae10f39e822a
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-04-13 15:29:19 +0530

    renamed the files

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Grid, Row, Col, Image} from 'react-bootstrap';
+// import styles from './UserSearch.css';
+
+let profile = () => {
+};
+
+
+
+export default profile;
---
+++ src/components/Repo/Repo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid, Row, Col, Image} from 'react-bootstrap';
+// import classes from './RepoSearch.css';
+
+
+const repo = () => {
+    
+};
+
+
+
+export default repo;
