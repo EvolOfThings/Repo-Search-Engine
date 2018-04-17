3ae451216d4ab7374af8199c39acc6d57028721c
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-04-17 20:50:21 +0530

    css module classname

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,17 +1,16 @@
 import React, {Component} from 'react';
 
-import './Home.css';
+import classes from './Home.css';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <h2>Git Repo Search</h2>
                 <div>
                     <label> Username </label>
-                    <input type='text' />
+                    <input className={classes.Input} type='text' />
                     <label>Repository</label>
-                    <input type='text' />
+                    <input className={classes.Input} type='text' />
                     {/* <button>Search</button> */}
                 </div>
              </div>
