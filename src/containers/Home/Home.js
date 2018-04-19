d13bcb00f741088b56d52c315bc77eb92a31bab5
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-04-19 10:13:02 +0530

    styling the search inputs

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,15 +5,13 @@ import classes from './Home.css';
 class Home extends Component {
     render() {
         return (
-            <div>
                 <div>
-                    <label> Username </label>
+                    <label> Username : </label>
                     <input className={classes.Input} type='text' />
-                    <label>Repository</label>
+                    <label>Repository :</label>
                     <input className={classes.Input} type='text' />
                     {/* <button>Search</button> */}
                 </div>
-             </div>
         );
     }
 }
