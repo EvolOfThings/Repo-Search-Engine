216c90f1ae625355975b18fa7e97be33c9f5ce2f
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-04-25 17:34:08 +0530

    backdrop css styling

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Backdrop.css';
+
+const backdrop = () => (
+    <div className={classes.Backdrop}>
+
+    </div>
+);
+
+export default backdrop;
