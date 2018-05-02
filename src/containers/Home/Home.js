a329a4bc74fed7c039bd1fa0729d0a285ffccaba
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-05-02 08:52:48 +0530

    GET method fetches the user data

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,14 +5,30 @@ import Repo from '../../components/Repo/Repo';
 import Modal from '../Modal/Modal';
 import Backdrop from '../../components/UI/Backdrop/Backdrop'
 import classes from './Home.css';
+import axios from 'axios';
 
 class Home extends Component {
     state = {
         username: '',
         repo: ''
     }
-getUser = () => {
-    const name = this.state 
+
+getUser = (username) => {
+    axios.get('https://api.github.com/users/' + username)
+    .then(response => {
+        console.log(response);
+        return response;
+    });
+}
+
+updateInputValue = (eve) => {
+    console.log(eve.target.value);
+    let inputUsername = eve.target.value;
+    this.getUser(inputUsername);
+
+    this.setState({
+        username: eve.target.value
+    });
 }
 
     render() {
@@ -20,9 +36,10 @@ getUser = () => {
         return (
                 <div>
                     <label> Username : </label>
-                    <input className={classes.Input} type='text' />
+                    <input className={classes.Input} type='text' value={this.state.username} onChange={this.updateInputValue}/>
+                    {/* <p style={{textAlign:'center',margin:'auto'}}>or</p>
                     <label>Repository :</label>
-                    <input className={classes.Input} type='text' onChange={(event) => this.setState({username: event.target.value})}/>
+                    <input className={classes.Input} type='text' onChange={(event) => this.setState({username: event.target.value})}/> */}
                     {/* <button>Search</button> */}
                     <Profile />
                     <Repo />
