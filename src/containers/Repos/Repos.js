57a370172255ea533cf45b0868e8d40488f5ed0f
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-05-08 11:31:06 +0530

    Repo container to list repos and exported into Profile

---
+++ src/containers/Repos/Repos.js
@@ -0,0 +1,21 @@
+import react, {Component} from 'react';
+
+class Repos extends Component {
+    state = {
+
+    }
+
+    render () {
+        return (
+            <div>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default Repos;
