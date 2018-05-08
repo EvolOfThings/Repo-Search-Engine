ed11d7006734d9cb42512afdaa4baa52957685ec
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-05-08 11:31:46 +0530

    renamed files

---
+++ src/components/RepoSearch/RepoSearch.js
@@ -0,0 +1,22 @@
+import React from 'react';
+// import {Grid, Row, Col, Image} from 'react-bootstrap';
+import classes from './Repo.css';
+
+
+const repo = (props) => (
+    <div>
+        <h4 style={{textAlign: 'center'}}>Repository</h4>
+        <div className={classes.RepoSearch}>
+            <img className={classes.ProfileImage} src={props.profileImage} alt="user profile image"/>
+            <section>
+            <p>Username: {props.username} </p>
+            <p>Repository: <a href={props.repoLink}>repo name</a></p>
+            <p>Issues: <a href='#'>Issues</a></p>
+            </section>
+        </div>
+    </div>
+);
+
+
+
+export default repo;
