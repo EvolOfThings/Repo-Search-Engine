c57e07b3a219773aef6d19c213f6ef61ad11b275
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-05-26 18:38:35 +0530

    keeping up with the streak

--- src/containers/Repos/Repos.js
+++ src/containers/Repos/Repos.js
@@ -7,15 +7,15 @@ class Repos extends Component {
 
     //get request for repos and map through it to put it in list
     //THIS IS WRONG
-    // getUser = (username) => { 
-    //     const urlLink = 'https://api.github.com/users/';
-    //    return axios.get(urlLink + username)
-    //     .then((response) => response.data)
-    //     .then(user => {
-    //         this.setState({
-    //             url: user.repos });
-    //     });
-    // }
+    getUser = (username) => { 
+        const urlLink = 'https://api.github.com/users/';
+       return axios.get(urlLink + username)
+        .then((response) => response.data)
+        .then(user => {
+            this.setState({
+                url: user.repos });
+        });
+    }
 
     render () {
         return (
